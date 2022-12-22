b17d98e67e893ed7f3c1fbe962b99116758f01fe
Chidinma <onyeobiakaris@gmail.com>
2022-12-21 19:17:12 -0800

    task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,19 +4,16 @@
  * @c: pointer to the string to convert
  * Return: the converted string.
 */
-char *string_toupper(char *str)
+char *string_toupper(char *c)
 {
 	int i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (c[i] != '\0')
 	{
-		if (str[i] >= 97 && str[i] <= 122)
-		{
-			str[i] = str[i] - 32;
-		}
+		if (c[i] >= 'a' && c[i] <= 'z')
+			c[i] -= 'a' - 'A';
 		i++;
 	}
-	return (str);
+	return (c);
 }
-	
