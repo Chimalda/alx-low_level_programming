f5d757ab8cc3cccebf05d9844641e05806bfedb5
Chidinma <onyeobiakaris@gmail.com>
2023-01-04 07:53:20 -0800

    task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_diagsums - prints diagonal sums of square matrix
  * @a: square matrix of integers
@@ -7,20 +8,11 @@
 */
 void print_diagsums(int *a, int size)
 {
-	int primC, sum1, sum2, diagC1, diagC2, row;
-
-
-	primC = 0, sum1 = 0, sum2 = 0, diagC1 = 0, diagC2 = 0, row = 0;
-	while (primC < size * size)
+	int b, sum1 = 0, sum2 = 0;
+	for (b = 0; b < size; b++)
 	{
-		if (primC == diagC1 + (row * size))
-			sum1 += a[primC];
-		if (primC == (size - diagC2 - 1) + (row * size))
-			sum2 += a[primC];
-		if (((primC + 1) % size) == 0)
-			row++, diagC1++, diagC2++;
-		primC++;
+		sum1 += a[(size + 1) * b];
+		sum2 += a[(size - 1) * (b + 1)];
 	}
-	printf("%d, ", sum1);
-	printf("%d\n", sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
