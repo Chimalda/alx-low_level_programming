aca8a450a4d5aa4ce4a271d13e675bf4529cd976
Chidinma <onyeobiakaris@gmail.com>
2023-03-05 05:36:25 -0800

    finally

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,6 @@
 #include "main.h"
 /**
+ * print_alphabet- check description
  *main- Entry Point
  *Return: Always 0
  */
@@ -8,8 +9,6 @@ void print_alphabet(void)
 	char letters;
 
 	for (letters = 'a'; letters <= 'z'; letters++)
-	
 		_putchar(letters);
-	
-	_putchar('\n');	
+	_putchar('\n');
 }
